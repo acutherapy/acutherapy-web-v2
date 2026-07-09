6d0a29a967849eeab984742c37771b04f1c461ca
acutherapy <acuherb@yahoo.com>
2026-07-09 13:54:19 -1000

    fix(seo): change parent schema type from Acupuncture to MedicalClinic in static generator script

--- generate-static.cjs
+++ generate-static.cjs
@@ -467,7 +467,7 @@ function buildJsonLd(route, description) {
   if (route === '/' || route === '') {
     const homeSchema = {
       '@context': 'https://schema.org',
-      '@type': 'Acupuncture',
+      '@type': 'MedicalClinic',
       'name': 'AcuTherapy Clinics',
       'image': 'https://acutherapy.com/acutherapy-clinic-honolulu.png',
       '@id': 'https://acutherapy.com',
